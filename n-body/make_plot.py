aad33865853e1d42674f030f0bf0e14eb146ba38
Jon <jonathan.hill.15@cnu.edu>
2019-11-13 15:42:11 -0500

    updated plot

--- n-body/make_plot.py
+++ n-body/make_plot.py
@@ -9,4 +9,9 @@ y = np.random.randn(n)
 fig = go.Figure()
 
 fig.add_trace(go.Scatter(x=x, y=y, mode='markers', name='test'))
+fig.update_layout(
+        title="Test Plot",
+        xaxis_title="X",
+        yaxis_title="Y")
+
 fig.write_html("test_plot.html")

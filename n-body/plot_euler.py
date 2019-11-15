f1af1879e2162fc19686922f8bbf919e081af122
Jon <jonathan.hill.15@cnu.edu>
2019-11-15 17:09:02 -0500

    improved euler animation

--- n-body/plot_euler.py
+++ n-body/plot_euler.py
@@ -1,5 +1,7 @@
-import numpy as np
+import pandas as pd
 import plotly.graph_objects as go
+import numpy as np
+# import plotly.graph_objects as go
 
 n = 100
 
@@ -8,49 +10,53 @@ y = np.exp(x)
 y0 = (0, 1)
 y1 = (1, 2)
 y2 = (2, 4)
-y3 = (3, 8) 
+y3 = (3, 8)
 
-# Create figure
-fig = go.Figure()
 
-# Add traces, one for each slider step
-# for step in np.arange(0, 5, 0.1):
-#     fig.add_trace(
-#         go.Scatter(
-#             visible=False,
-#             line=dict(color="#00CED1", width=6),
-#             name="𝜈 = " + str(step),
-#             x=np.arange(0, 10, 0.01),
-#             y=np.sin(step * np.arange(0, 10, 0.01))))
+# fig.add_trace(go.Scatter(visible=True, name="Truth", x=x, y=y))
+# fig.add_trace(go.Scatter(visible=False, name="Step 1", line=dict(color='red'), x=[y0[0],y1[0]], y=[y0[1], y1[1]]))
+# fig.add_trace(go.Scatter(visible=False, name="Step 2", line=dict(color='purple'), x=[y1[0],y2[0]], y=[y1[1], y2[1]]))
+# fig.add_trace(go.Scatter(visible=False, name="Step 3", line=dict(color='green'), x=[y2[0],y3[0]], y=[y2[1], y3[1]]))
 
-fig.add_trace(go.Scatter(visible=True, name="Truth", x=x, y=y))
-fig.add_trace(go.Scatter(visible=False, name="Step 1", line=dict(color='red'), x=[y0[0],y1[0]], y=[y0[1], y1[1]]))
-fig.add_trace(go.Scatter(visible=False, name="Step 2", line=dict(color='purple'), x=[y1[0],y2[0]], y=[y1[1], y2[1]]))
-fig.add_trace(go.Scatter(visible=False, name="Step 3", line=dict(color='green'), x=[y2[0],y3[0]], y=[y2[1], y3[1]]))
+f1 = go.Frame(name ="f1", data=[go.Scatter(x=x, y=y, line=dict(color='purple'))])
+f2 = go.Frame(name ="f2", data=[go.Scatter(visible=True, name="Step 1", line=dict(color='red'), x=[y0[0],y1[0]], y=[y0[1], y1[1]])])
+f3 = go.Frame(name ="f3", data=[go.Scatter(visible=True,name="Step 2", line=dict(color='purple'), x=[y1[0],y2[0]], y=[y1[1], y2[1]])])
+f4 = go.Frame(name ="f4", data=[go.Scatter(name="Step 3", line=dict(color='green'), x=[y2[0],y3[0]], y=[y2[1], y3[1]])])
 
-# Make 10th trace visible
-# fig.data[10].visible = True
+frames = [f1, f2, f3, f4]
+
+
+
 
 # Create and add slider
 steps = []
-for i in range(len(fig.data)):
+# for i in range(len(fig.data)):
+for i, frame in enumerate(frames):
+    # step = dict(
+    #     method="restyle",
+    #     args=["visible", [True] * (i+1) + [False] * (len(fig.data)-i-1)],
+    #     name = "0"
+    # )
     step = dict(
-        method="restyle",
-        args=["visible", [True] * (i+1) + [False] * (len(fig.data)-i-1)],
-        name = "0"
+        method="animate",
+        args=[[frame.name],{"frame": {"duration": 300, "redraw": False}, "mode": "immediate", "transition": {"duration": 300}}],
+        label = i,
+        name=frame.name
     )
-    # step["args"][1][i] = True  # Toggle i'th trace to "visible"
+# #     # step["args"][1][i] = True  # Toggle i'th trace to "visible"
     steps.append(step)
 
+
 sliders = [dict(
-    active=10,
+    active=0,
     currentvalue={"prefix": "Step: "},
-    pad={"t": len(fig.data)},
+    # pad={"t": len(fig.data)},
     transition={"duration": 300, "easing": "cubic-in-out"},
     steps=steps
 )]
 
-fig.update_layout(
+
+layout = dict(
     sliders=sliders,
     hovermode="closest",
     title=r'$\text{Euler Method for:    } y=e^t$',
@@ -63,43 +69,13 @@ fig.update_layout(
 
 )
 
-# fig.show()
+# Create figure
+# fig = go.Figure(data=[go.Scatter(visible=True, name="Truth", x=x, y=y),go.Scatter(visible=False, name="Step 1", line=dict(color='red'), x=[y0[0],y1[0]], y=[y0[1], y1[1]]), go.Scatter(visible=False,name="Step 2", line=dict(color='purple'), x=[y1[0],y2[0]], y=[y1[1], y2[1]]), go.Scatter(visible=False,name="Step 3", line=dict(color='green'), x=[y2[0],y3[0]], y=[y2[1], y3[1]])],
+fig = go.Figure(data=[go.Scatter(visible=True, name="Truth", x=x, y=y, line=dict(color='purple')), go.Scatter(visible=True, name="Truth", x=x, y=y, line=dict(color='purple'))],  
+  frames=frames, 
+  layout=layout)
 
+fig.show()
 
-
-
-
-
-
-
-
-# fig = go.Figure(
-#     data=[go.Scatter(x=x, y=y,
-#                      mode="lines",
-#                      line=dict(width=2, color="blue")),
-#           go.Scatter(x=x, y=y,
-#                      mode="lines",
-#                      line=dict(width=2, color="blue"))],
-#     layout=go.Layout(
-#         xaxis=dict(range=[-1, 3], autorange=False, zeroline=False),
-#         yaxis=dict(range=[0, 9], autorange=False, zeroline=False),
-#         title_text="Kinematic Generation of a Planar Curve", hovermode="closest",
-#         updatemenus=[dict(type="buttons",
-#                           buttons=[dict(label="Play",
-#                                         method="animate",
-#                                         args=[None])])]),
-#     # frames=[go.Frame(
-#     #     data=[go.Scatter(
-#     #         x=[x[k], 1, None,x[k], 2 ],
-#     #         y=[y[k], 2, None, y[k], 3],
-#     #         mode="lines",
-#     #         marker=dict(color="red", size=10))])
-#     #         for k in range(n)]
-#     frames=[go.Frame(data=[go.Scatter(x=[1, 1.5], y=[1, 2], mode='lines', line=dict(color='red'), name="yup")]),
-#             go.Frame(data=[go.Scatter(x=[1, 4, None, 2, 2], y=[1, 4, None, 3, 5], mode='lines', line=dict(color='red'))]),
-#             go.Frame(data=[go.Scatter(x=[1, 4, None, 2, 2], y=[1, 4, None, 4, 6], mode='lines', line=dict(color='red'))])]
-
-
-# )
 
 fig.write_html("euler.html", include_mathjax='cdn')
